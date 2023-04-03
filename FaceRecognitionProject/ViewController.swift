bd8f478482c7707fbb86842665e8e48eb8d9784e
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-04-03 07:12:48 -0600

    example on how to use biometrics with swift

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.messageLabel.text = ""
     }
 
-
+    @IBAction func signIn(_ sender: Any) {
+        let authContext = LAContext()
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { success, error in
+                DispatchQueue.main.async {
+                    if success == true {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                        self.messageLabel.text = ""
+                    } else {
+                        self.messageLabel.text = "Error with authentication!!"
+                    }
+                }
+            }
+        } else {
+            self.messageLabel.text = "No biometric's detected"
+        }
+    }
+    
 }
 
